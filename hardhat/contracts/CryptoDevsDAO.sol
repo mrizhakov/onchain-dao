7cbdff3724f8e8a91940678d5f6bcb4627f62eeb
Michael Rizhakov <m.rizhakov@gmail.com>
2024-02-29 19:18:03 +0100

    Added a simple NFT marketplace prototype

---
+++ hardhat/contracts/CryptoDevsDAO.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// Add interfaces
+
+contract CryptoDevsDAO is Ownable {
+    //Contract code goes here
+}
